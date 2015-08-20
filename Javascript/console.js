f0c659da91fc0f2d92630e2f4bff8139fcd080d2
Shining Dragon <speakwithjoe@hotmail.co.uk>
2015-08-20 14:39:46 +0200

    Refactoring

--- Javascript/console.js
+++ Javascript/console.js
@@ -1,11 +1,15 @@
 ﻿var downcount = require('./js/downcount.js');
 var prompt = require('prompt');
+var async = require("async");
 
 console.log('Welcome to Downcount');
 
 PlayGame();
 
 
+///
+/// Play Downcount
+///
 function PlayGame() {
     console.log('');
 
@@ -24,26 +28,24 @@ function PlayGame() {
     };
     
     prompt.start();
-    var integers;
-    var targetNumber;
     prompt.get(schema, function (err, result) {
         if (err) { return onErr(err); }
         
         // Validate the input
-        targetNumber = parseInt(result.targetNumber);
+        var targetNumber = parseInt(result.targetNumber);
         if (isNaN(targetNumber)) {
-            return onErr('Invalid target number entered')
+            return onErr('Invalid target number entered');
         }
 
-        integers = result.integers;
+        var integers = IntegerArrayFromString(result.integers);
         
-        console.log('The game is to find ' + targetNumber + ' from the set of numbers ' + integers 
-            + ', you have 30 seconds starting now');
+        console.log('The game is to find ' + targetNumber + ' from the set of numbers ' + 
+            integers + ', you have 30 seconds starting now');
         console.log('');
         
         // This should run async
         var solver = new downcount.DownCountSolver();
-        var solutions = solver.Solve(targetNumber, integers, false);
+        var solutions = solver.Solve(targetNumber, integers, true, false);
         
         var tick = 30;
         var interval = setInterval(function () {
@@ -54,30 +56,69 @@ function PlayGame() {
             if (tick == -1) {
                 clearInterval(interval);
                 console.log('');
-                console.log('');
-                if (solutions.Equations.length > 0) {
-                    console.log('Downcount found an exact solution');
-                    console.log(solutions.Equations[0].toString() + ' = ' + solutions.Equations[0].Value());
-                }
-                else {
-                    console.log('Downcount couldn\'t find an exact solution');
-                }
+                ShowResults(solutions, targetNumber);
                 
-                // Shall we play again
-                console.log('');
-                console.log('Do you wish to play again, Y/N');
-                prompt.start();
-                prompt.get(['playAgain'], function (err, result) {
-                    if (err) { return onErr(err); }
-                    if (result.playAgain == 'y' || result.playAgain == 'Y') {
-                        PlayGame();
-                    }
-                });
             }
         }, 1000);        
     });
 }
 
+///
+/// Analyse the solutions returned by downcount and display to the user
+///
+function ShowResults(solutions, targetNumber) {
+    console.log('');    
+    if (solutions.Equations.length > 0) {
+        if (solutions.Equations[0].Value() == targetNumber) {
+            console.log('Downcount found an exact solution');
+            console.log(solutions.Equations[0].toString() + ' = ' + solutions.Equations[0].Value());
+        }
+        else {
+            var foundValue = solutions.Equations[0].Value();
+            console.log('Downcount couldn\'t find an exact solution but found a solution within ' + (targetNumber - foundValue) + ' of ' + targetNumber);
+            console.log(solutions.Equations[0].toString() + ' = ' + foundValue);
+        }
+    }
+    else {
+        console.log('Downcount couldn\'t find an exact or close solution');
+    }
+    
+    // Shall we play again
+    console.log('');
+    console.log('Do you wish to play again, Y/N');
+    prompt.start();
+    prompt.get(['playAgain'], function (err, result) {
+        if (err) { return onErr(err); }
+        if (result.playAgain == 'y' || result.playAgain == 'Y') {
+            PlayGame();
+        }
+    });
+}
+
+///
+/// Return an array from a comman seperated list of integers
+///
+function IntegerArrayFromString(numbers) {
+    if (typeof numbers !== 'string') {
+        throw "IntegerSet: numbers not a string";
+    }
+    
+    var integerSet = [];
+    var res = numbers.split(",");
+    for (var i = 0; i < res.length; ++i) {
+        var number = parseInt(res[i]);
+        if (isNaN(number)) {
+            throw "Invalid number creating IntegerSet";
+        }
+        integerSet.push(number);
+    }
+
+    return integerSet;
+}
+
+///
+/// Handle errors
+///
 function onErr(err) {
     console.log('Downcount encountered an error: ' + err);
     return 1;
--- Javascript/js/downcount.js
+++ Javascript/js/downcount.js
@@ -1,27 +1,15 @@
 'use strict';
 
 var IntegerSet = function IntegerSet(numbers) {
-    this.Numbers = []; 
-}
-
-IntegerSet.prototype.FromString = function (numbers) {
-    if (typeof numbers !== 'string') {
-        throw "IntegerSet: numbers not a string"
+    this.Numbers = [];
+    for (var i = 0; i < numbers.length; ++i) {
+        this.Add(numbers[i]);
     }
-    
-    var res = numbers.split(",");
-    for (var i = 0; i < res.length; ++i) {
-        var number = parseInt(res[i]);
-        if (isNaN(number)) {
-            throw "Invalid number creating IntegerSet"
-        }
-        this.Numbers.push(number)
-    }
-}
+};
 
 IntegerSet.prototype.Add = function (x) {
     if (typeof x !== 'number' && Math.floor(x) !== x) {
-        throw "Trying to add a non number to the IntegerSet"
+        throw "Trying to add a non number to the IntegerSet";
     }
 
     this.Numbers.push(x);
@@ -30,52 +18,45 @@ IntegerSet.prototype.Add = function (x) {
 IntegerSet.prototype.SubsetNotIncluding = function (n) {
     
     if (typeof n !== 'number' && Math.floor(n) !== n) {
-        throw "SubsetNotIncluding: n not an integer"
+        throw "SubsetNotIncluding: n not an integer";
     }
     
     if (n < 0 || n > this.Numbers.length - 1) {
-        throw "SubsetNotIncluding: n out of range"
+        throw "SubsetNotIncluding: n out of range";
     }
-
-
-    var subset = new IntegerSet();
+    
+    
+    var subset = [];
     for (var i = 0; i < this.Numbers.length; ++i) {
         if (i != n) {
-            subset.Add(this.Numbers[i]);
+            subset.push(this.Numbers[i]);
         }
     }
-
-    return subset;
-}
+    
+    return new IntegerSet(subset);
+};
 
 var DownCountGame = function DownCountGame() {
-    this.TargetNumber;   
-    this.SourceNumbers;
-}
+    this.TargetNumber = 0;
+    this.SourceNumbers = new IntegerSet([]);
+};
 
-DownCountGame.prototype.Init = function(targetNumber, sourceNumbers) {
+DownCountGame.prototype.Init = function (targetNumber, sourceNumbers) {
     
     if (typeof targetNumber !== 'number' || Math.floor(targetNumber) !== targetNumber) {
-        throw "DownCountGame: targetNumber not an integer"
+        throw "DownCountGame: targetNumber not an integer";
     }
     
     this.TargetNumber = targetNumber;
     
-    this.SourceNumbers = new IntegerSet();
-    this.SourceNumbers.FromString(sourceNumbers);
-}
+    this.SourceNumbers = new IntegerSet(sourceNumbers);
+};
 
-var AddOperator = function AddOperator(lhs, rhs)
-{
-    //if((typeof lhs != 'AddOperator' && typeof lhs != 'MinusOperator' && typeof lhs != 'MultiplyOperator' && typeof lhs != 'DivideOperator' && typeof lhs != 'number') ||
-    //    (typeof rhs != 'AddOperator' && typeof rhs != 'MinusOperator' && typeof rhs != 'MultiplyOperator' && typeof rhs != 'DivideOperator' && typeof rhs != 'number')) {
-    //        throw "Not a valid Operator"
-    //}
-
+var AddOperator = function AddOperator(lhs, rhs) {
     this.LHS = lhs;
     this.RHS = rhs;
     this.Symbol = '+';
-}
+};
 
 AddOperator.prototype.Value = function () {
     var valLHS;
@@ -91,25 +72,20 @@ AddOperator.prototype.Value = function () {
     } else {
         valRHS = this.RHS.Value();
     }
-
+    
     return valLHS + valRHS;
-}
+};
 
-AddOperator.prototype.toString = function () {  
+AddOperator.prototype.toString = function () {
     return '(' + this.LHS.toString() + this.Symbol + this.RHS.toString() + ')';
-}
+};
 
 var MinusOperator = function MinusOperator(lhs, rhs) {
-
-    //if ((typeof lhs != 'AddOperator' && typeof lhs != 'MinusOperator' && typeof lhs != 'MultiplyOperator' && typeof lhs != 'DivideOperator' && typeof lhs != 'number') ||
-    //    (typeof rhs != 'AddOperator' && typeof rhs != 'MinusOperator' && typeof rhs != 'MultiplyOperator' && typeof rhs != 'DivideOperator' && typeof rhs != 'number')) {
-    //    throw "Not a valid Operator"
-    //}
-
+    
     this.LHS = lhs;
     this.RHS = rhs;
     this.Symbol = '-';
-}
+};
 
 MinusOperator.prototype.Value = function () {
     var valLHS;
@@ -127,23 +103,18 @@ MinusOperator.prototype.Value = function () {
     }
     
     return valLHS - valRHS;
-}
+};
 
 MinusOperator.prototype.toString = function () {
     return '(' + this.LHS.toString() + this.Symbol + this.RHS.toString() + ')';
-}
+};
 
 var MultiplyOperator = function MultiplyOperator(lhs, rhs) {
-
-    //if ((typeof lhs != 'AddOperator' && typeof lhs != 'MinusOperator' && typeof lhs != 'MultiplyOperator' && typeof lhs != 'DivideOperator' && typeof lhs != 'number') ||
-    //    (typeof rhs != 'AddOperator' && typeof rhs != 'MinusOperator' && typeof rhs != 'MultiplyOperator' && typeof rhs != 'DivideOperator' && typeof rhs != 'number')) {
-    //    throw "Not a valid Operator"
-    //}
-
+    
     this.LHS = lhs;
     this.RHS = rhs;
     this.Symbol = 'x';
-}
+};
 
 MultiplyOperator.prototype.Value = function () {
     var valLHS;
@@ -161,22 +132,18 @@ MultiplyOperator.prototype.Value = function () {
     }
     
     return valLHS * valRHS;
-}
+};
 
 MultiplyOperator.prototype.toString = function () {
     return '(' + this.LHS.toString() + this.Symbol + this.RHS.toString() + ')';
-}
+};
 
 var DivideOperator = function DivideOperator(lhs, rhs) {
     
-    //if ((typeof lhs != 'AddOperator' && typeof lhs != 'MinusOperator' && typeof lhs != 'MultiplyOperator' && typeof lhs != 'DivideOperator' && typeof lhs != 'number') ||
-    //    (typeof rhs != 'AddOperator' && typeof rhs != 'MinusOperator' && typeof rhs != 'MultiplyOperator' && typeof rhs != 'DivideOperator' && typeof rhs != 'number')) {
-    //    throw "Not a valid Operator"
-    //}
     this.LHS = lhs;
     this.RHS = rhs;
     this.Symbol = '/';
-}
+};
 
 DivideOperator.prototype.Value = function () {
     var valLHS;
@@ -194,33 +161,33 @@ DivideOperator.prototype.Value = function () {
     }
     
     return valLHS / valRHS;
-}
+};
 
 DivideOperator.prototype.toString = function () {
     return '(' + this.LHS.toString() + this.Symbol + this.RHS.toString() + ')';
-}
+};
 
 var Solutions = function Solutions() {
     this.Equations = [];
-}
+};
 
-Solutions.prototype.Add = function(equation) {
-    for (var i= 0; i < this.Equations.length; ++i) {
+Solutions.prototype.Add = function (equation) {
+    for (var i = 0; i < this.Equations.length; ++i) {
         this.Equations[i] = new AddOperator(this.Equations[i], equation);
     }
-}
+};
 
 Solutions.prototype.Minus = function (equation) {
     for (var i = 0; i < this.Equations.length; ++i) {
         this.Equations[i] = new MinusOperator(this.Equations[i], equation);
     }
-}
+};
 
 Solutions.prototype.Multiply = function (equation) {
     for (var i = 0; i < this.Equations.length; ++i) {
         this.Equations[i] = new MultiplyOperator(this.Equations[i], equation);
     }
-}
+};
 
 Solutions.prototype.Divide = function (equation) {
     for (var i = 0; i < this.Equations.length; ++i) {
@@ -228,26 +195,60 @@ Solutions.prototype.Divide = function (equation) {
             this.Equations[i] = new DivideOperator(this.Equations[i], equation);
         }
     }
-}
+};
 
-var DownCountSolver = function DownCountSolver(){
-    
-    this.Game;
+var DownCountSolver = function DownCountSolver() {    
+};
 
-}
-
-DownCountSolver.prototype.Solve = function (targetNumber, sourceNumbers, allSolutions) {
+DownCountSolver.prototype.Solve = function (targetNumber, sourceNumbers, closeSolutions, allSolutions) {
     
     if (typeof allSolutions !== 'boolean' ) {
-        throw "Invalid allSolutions";
+        throw "allSolutions not a boolean";
+    }
+    
+    if (typeof closeSolutions !== 'boolean') {
+        throw "closeSolutions not a boolean";
+    }
+    
+    if (typeof targetNumber !== 'number' || Math.floor(targetNumber) !== targetNumber) {
+        throw "targetNumber not an integer";
     }
     
     var game = new DownCountGame();
     game.Init(targetNumber, sourceNumbers);
 
-    return this.SolveGame(game, allSolutions);
+    var solutions = new Solutions();
+
+    solutions = this.SolveGame(game, allSolutions);
+    if (solutions.Equations.length > 0) {
+        // We found some exact solutions so we can return now
+        return solutions;
+    }
+
+    if (closeSolutions) {
+        // Try to find some close solutions
+        var originalTargetNumber = game.TargetNumber;
+        for (var i = 1; i <= 10; ++i) {
+            game.TargetNumber = originalTargetNumber + i;
+            solutions = this.SolveGame(game, false);
+            if (solutions.Equations.length > 0) {
+                return solutions;
+            }
+
+            game.TargetNumber = originalTargetNumber - i;
+            solutions = this.SolveGame(game, false);
+            if (solutions.Equations.length > 0) {
+                return solutions;
+            }
+        }
+    }
+
+    return solutions;
 };
 
+///
+/// Return only exact solutions for the game
+///
 DownCountSolver.prototype.SolveGame = function (game, allSolutions) {
     
     if (typeof allSolutions !== 'boolean') {
@@ -255,7 +256,7 @@ DownCountSolver.prototype.SolveGame = function (game, allSolutions) {
     }
     
     var solutions = new Solutions();
-
+    
     if (game.SourceNumbers.Numbers.indexOf(game.TargetNumber) >= 0) {
         solutions.Equations.push(game.TargetNumber);
     }
@@ -265,77 +266,83 @@ DownCountSolver.prototype.SolveGame = function (game, allSolutions) {
     }
     
     
-    if (game.SourceNumbers.Numbers.length == 0) {
+    if (game.SourceNumbers.Numbers.length === 0) {
         return solutions;
     }
     
     for (var i = 0; i < game.SourceNumbers.Numbers.length; ++i) {
-        var solutions = this.SolveSubset(game, i, allSolutions)
+        var solutionsSubset = this.SolveSubset(game, i, allSolutions);
+        for (var j = 0; j < solutionsSubset.Equations.length; ++j) {
+            solutions.Equations.push(solutionsSubset.Equations[j]);
+        }
         
         if (!allSolutions && solutions.Equations.length > 0)
             return solutions;
     }
     
     return solutions;
-}
+};
 
-DownCountSolver.prototype.SolveSubset = function (game, i, allSolutions) {
-
+///
+/// Solve the nth subset of the given game
+///
+DownCountSolver.prototype.SolveSubset = function (game, n, allSolutions) {
+    
     var solutions = new Solutions();
-    var currentNumber = game.SourceNumbers.Numbers[i];
-    var subset = game.SourceNumbers.SubsetNotIncluding(i);
-
+    var currentNumber = game.SourceNumbers.Numbers[n];
+    var subset = game.SourceNumbers.SubsetNotIncluding(n);
+    
     var gameAdd = new DownCountGame();
     gameAdd.TargetNumber = game.TargetNumber - currentNumber;
     gameAdd.SourceNumbers = subset;
     var solutionsAdd = this.SolveGame(gameAdd, allSolutions);
     solutionsAdd.Add(currentNumber);
     for (var i = 0; i < solutionsAdd.Equations.length; ++i) {
-        solutions.Equations.push(solutionsAdd.Equations[i])
+        solutions.Equations.push(solutionsAdd.Equations[i]);
     }
     if (!allSolutions && solutions.Equations.length > 0) {
         return solutions;
     }
-
+    
     var gameMinus = new DownCountGame();
     gameMinus.TargetNumber = game.TargetNumber + currentNumber;
     gameMinus.SourceNumbers = subset;
     var solutionsMinus = this.SolveGame(gameMinus, allSolutions);
     solutionsMinus.Minus(currentNumber);
-    for (var i = 0; i < solutionsMinus.Equations.length; ++i) {
-        solutions.Equations.push(solutionsMinus.Equations[i])
+    for (i = 0; i < solutionsMinus.Equations.length; ++i) {
+        solutions.Equations.push(solutionsMinus.Equations[i]);
     }
     if (!allSolutions && solutions.Equations.length > 0) {
         return solutions;
     }
-
+    
     var gameMultiply = new DownCountGame();
-    if (game.TargetNumber % currentNumber == 0) {
+    if (game.TargetNumber % currentNumber === 0) {
         gameMultiply.TargetNumber = game.TargetNumber / currentNumber;
         gameMultiply.SourceNumbers = subset;
         var solutionsMultiply = this.SolveGame(gameMultiply, allSolutions);
         solutionsMultiply.Multiply(currentNumber);
-        for (var i = 0; i < solutionsMultiply.Equations.length; ++i) {
-            solutions.Equations.push(solutionsMultiply.Equations[i])
+        for (i = 0; i < solutionsMultiply.Equations.length; ++i) {
+            solutions.Equations.push(solutionsMultiply.Equations[i]);
         }
         if (!allSolutions && solutions.Equations.length > 0) {
             return solutions;
         }
     }
-
+    
     var gameDivide = new DownCountGame();
     gameDivide.TargetNumber = game.TargetNumber * currentNumber;
     gameDivide.SourceNumbers = subset;
     var solutionsDivide = this.SolveGame(gameDivide, allSolutions);
     solutionsDivide.Divide(currentNumber);
-    for (var i = 0; i < solutionsDivide.Equations.length; ++i) {
-        solutions.Equations.push(solutionsDivide.Equations[i])
+    for (i = 0; i < solutionsDivide.Equations.length; ++i) {
+        solutions.Equations.push(solutionsDivide.Equations[i]);
     }
     if (!allSolutions && solutions.Equations.length > 0) {
         return solutions;
     }
-
+    
     return solutions;
-}
+};
 
 module.exports.DownCountSolver = DownCountSolver;
